1ca27584d9b466036cf4abe25029dca9c973100c
codyw9524 <codyw9524@gmail.com>
2016-09-07 14:14:51 -0500

    created models (admins, users, teams, teamcaptains, events)

---
+++ db/migrate/20160907191053_create_team_captains.rb
@@ -0,0 +1,10 @@
+class CreateTeamCaptains < ActiveRecord::Migration[5.0]
+  def change
+    create_table :team_captains do |t|
+      t.references :user, foreign_key: true
+      t.references :team, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
