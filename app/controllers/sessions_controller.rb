6e60d513727565e86a9e332092b83a7f3c5e54f0
codyw9524 <codyw9524@gmail.com>
2016-09-30 11:30:54 -0500

    added Terrys about page changes and initial redesign of events page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,10 +3,11 @@ class SessionsController < ApplicationController
 		user = User.find_by(email: params[:email])
 		if user && user.authenticate(params[:password])
 			session[:user_id] = user.id
+			redirect_to '/teams'
 		else
 			flash[:login_errors] = "Invalid Credentials."
+			redirect_back fallback_location: '/'
 		end
-		redirect_to :back
 	end
 	def destroy
 		reset_session
