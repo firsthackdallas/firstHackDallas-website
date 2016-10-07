f675ba068a3b66daff5a23a18db3c1e3f9bd914d
codyw9524 <codyw9524@gmail.com>
2016-10-07 09:35:48 -0500

    added validations to moblie login

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -12,18 +12,16 @@ $(document).on("turbolinks:load", function(){
   $('#close_new_team_form').click(function(){
   	$("#new_team_form").hide();
 		$(".new_team_link").show();
-		e.preventDefault();
   })
   $('.new_team_link').click(function(){
     $(".new_team_link").hide();
     $("#new_team_form").show();
-    e.preventDefault();
   })
   $(".button-collapse").sideNav();
-  $("#login_form").submit(function(){
+  $(".login_form").submit(function(){
     $.post($(this).attr('action'), $(this).serialize(), function(json){
       if(json.error){
-        $('#login_error_p').html(json.error.msg)
+        $('.login_error_p').html(json.error.msg)
       }
     })
     return false;
