3c3a9b21f18fa47e1d4efeda89f970a8c55b468a
codyw9524 <codyw9524@gmail.com>
2016-11-01 09:41:18 -0500

    updated admin dashboard so Registered Users shows users who are not admins and not on the wait list

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -1,7 +1,9 @@
 class AdminsController < ApplicationController
 	before_action :require_admin
 	def index
-		@users = User.all
+		admins = Admin.joins(:user).pluck('user_id')
+		waitlist = WaitList.joins(:user).pluck('user_id')
+		@users = User.all.where.not(id: admins).where.not(id: waitlist)
 		@teams = Team.all
 		@admins = Admin.all
 		@wait_list = WaitList.all
